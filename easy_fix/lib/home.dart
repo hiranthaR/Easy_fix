027c260f5c7b402cb7fbd124891d218240589a66
Bhanuka <bhanuka9519@gmail.com>
2019-10-07 16:26:03 +0530

    get current location

--- easy_fix/lib/home.dart
+++ easy_fix/lib/home.dart
@@ -7,18 +7,62 @@ import 'package:easy_fix/payment.dart';
 import 'package:easy_fix/setting.dart';
 import 'package:easy_fix/yourworks.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-class HomePage extends StatefulWidget{
+import 'package:location/location.dart';
+
+class HomePage extends StatefulWidget {
   @override
   State createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>{
-  Completer<GoogleMapController> _controller = Completer();
+class _HomePageState extends State<HomePage> {
+  GoogleMapController _controller;
+
+  Location _locationService = Location();
+  bool _permission = false;
+  Marker _marker;
 
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
+
+  initState() {
+    super.initState();
+    initLocation();
+  }
+
+  initLocation() async {
+    try {
+      bool serviceStatus = await _locationService.serviceEnabled();
+      print("Service status: $serviceStatus");
+      if (serviceStatus) {
+        _permission = await _locationService.requestPermission();
+        print("Permission: $_permission");
+        if (_permission) {
+          final location = await _locationService.getLocation();
+
+          final _currentCameraPosition = CameraPosition(
+              target: LatLng(location.latitude, location.longitude), zoom: 16);
+
+          _controller.animateCamera(
+              CameraUpdate.newCameraPosition(_currentCameraPosition));
+          setState(() {
+            _marker = Marker(
+                markerId: MarkerId("fuck"),
+                position: LatLng(location.latitude, location.longitude));
+          });
+        }
+      } else {
+        bool serviceStatusResult = await _locationService.requestService();
+        print("Service status activated after request: $serviceStatusResult");
+        if (serviceStatusResult) {
+          initLocation();
+        }
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
 
   GlobalKey<ScaffoldState> _sacaffoldkey = new GlobalKey();
   @override
@@ -29,29 +73,35 @@ class _HomePageState extends State<HomePage>{
         centerTitle: true,
         backgroundColor: Colors.transparent,
         elevation: 0.0,
-        title: new Text("Easy Fix",style: TextStyle(color: Colors.orange),
+        title: new Text(
+          "Easy Fix",
+          style: TextStyle(color: Colors.orange),
         ),
-        leading: IconButton(icon: Icon(Icons.menu),color: Colors.black, onPressed: (){
-          _sacaffoldkey.currentState.openDrawer();
-        }
-        ),
+        leading: IconButton(
+            icon: Icon(Icons.menu),
+            color: Colors.black,
+            onPressed: () {
+              _sacaffoldkey.currentState.openDrawer();
+            }),
       ),
       body: GoogleMap(
         mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
+          _controller = controller;
         },
+        markers: _marker != null ? Set.of([_marker]) : Set(),
       ),
       drawer: Drawer(
-        child:ListView(
+        child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
             DrawerHeader(
-              child: Text("Your Profile",
-              style: TextStyle(
-                color: Colors.redAccent,
-              ),
+              child: Text(
+                "Your Profile",
+                style: TextStyle(
+                  color: Colors.redAccent,
+                ),
               ),
               decoration: BoxDecoration(
                 color: new Color(0xff622F74),
@@ -59,41 +109,42 @@ class _HomePageState extends State<HomePage>{
             ),
             ListTile(
               title: Text("Your Works"),
-              onTap: (){
-                Navigator.push(
-              context, MaterialPageRoute(builder: (context) => WorksPage()));
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => WorksPage()));
               },
             ),
             ListTile(
               title: Text("Help"),
-              onTap: (){
-                Navigator.push(
-              context, MaterialPageRoute(builder: (context) => HelpPage()));
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => HelpPage()));
               },
             ),
             ListTile(
               title: Text("Payment"),
-              onTap: (){
-                Navigator.push(
-              context, MaterialPageRoute(builder: (context) => PaymentPage()));
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => PaymentPage()));
               },
             ),
-            ListTile(title: Text("Free Works"),
-            onTap: (){
-                Navigator.push(
-              context, MaterialPageRoute(builder: (context) => FreeWorkPage()));
+            ListTile(
+              title: Text("Free Works"),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => FreeWorkPage()));
               },
             ),
             ListTile(
               title: Text("Setting"),
-              onTap: (){
-                Navigator.push(
-              context, MaterialPageRoute(builder: (context) => SettingPage()));
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SettingPage()));
               },
             ),
           ],
-          ) ,
-          ),
-      );
+        ),
+      ),
+    );
   }
 }
